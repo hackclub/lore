9f709ba76c4e320cadceb825ec5b18866b9ef1fd
Abby <abigail9664@gmail.com>
2020-08-08 21:55:23 +0000

    currently does a hello world

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const { App } = require('@slack/bolt')
-console.log(process.env.PORT)
-// const app = new App({
-//   signingSecret: process.env.SIGNING_SECRET,
-//   token: process.env.BOT_TOKEN
-// });
+const token = process.env.BOT_TOKEN
+const app = new App({
+	signingSecret: process.env.SIGNING_SECRET,
+	token: token
+});
 
-// (async () => {
-//   await app.start(process.env.PORT || 3000);
-//   // publishMessage('C018J1ZEFA7', 'sick it works')
-//   console.log('⚡️ Bolt app is running!')
-// })()
+async function publishMessage(id, text) {
+	try {
+		const result = await app.client.chat.postMessage({
+			token: token,
+			channel: id,
+			text: text
+		});
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+(async () => {
+	try {
+		await app.start(process.env.PORT || 3000);
+		publishMessage('C018J1ZEFA7', 'sick it works')
+		console.log('⚡️ Bolt app is running!')
+	} catch (err) {
+		console.error(err);
+	}
+})()
