0abcf763d693881d3197f82f41724a8a1e6271ba
Abby W <abigail9664@gmail.com>
2020-08-08 14:19:06 -0700

    nowhere close to finish, moving to repl.it

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Hack Club Theme Starter
+# (Will change the README later)
 
 A sample [Next.js] project for getting started with [MDX], [Theme UI], & [Hack Club Theme].
 
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Container, Box, Text, Heading } from 'theme-ui'
+import fetch from 'isomorphic-unfetch'
+
+export default () => {
+  return <Container>
+      <Heading as="h1" variant="title">
+          Design Library
+      </Heading>
+      <Text>They're gonna be great</Text>
+  </Container>
+}
