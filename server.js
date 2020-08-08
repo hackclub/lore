679a66cfeb9ef2227b1cb187ccd0c6bdb74fa1fd
Abby <abigail9664@gmail.com>
2020-08-08 23:33:39 +0000

    add code to find URLs in messages

--- server.js
+++ server.js
@@ -17,10 +17,21 @@ async function publishMessage(id, text) {
 	}
 }
 
+const hasUrl = (message) => (
+	new RegExp(
+		'([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?'
+	).test(message)
+);
+
+app.event('message', async (body) => {
+	if (hasUrl(body.event.text)) {
+		publishMessage('C018J1ZEFA7', 'sick it works')
+	}
+});
+
 (async () => {
 	try {
 		await app.start(process.env.PORT || 3000);
-		publishMessage('C018J1ZEFA7', 'sick it works')
 		console.log('⚡️ Bolt app is running!')
 	} catch (err) {
 		console.error(err);
