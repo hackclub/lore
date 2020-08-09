6311ed4352da85b87f3239d88b303a7b26db3c35
Abby <abigail9664@gmail.com>
2020-08-09 23:11:47 +0000

    add code to store link in airtable

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const { App } = require('@slack/bolt')
+const Airtable = require('airtable');
+//use metascraper https://www.npmjs.com/package/scrape-meta
 const token = process.env.BOT_TOKEN
 const app = new App({
 	signingSecret: process.env.SIGNING_SECRET,
 	token: token
 });
+Airtable.configure({
+	endpointUrl:
+		'https://api.airtable.com',
+	apiKey: process.env.AIRTABLE_API_KEY
+});
+
+var base = Airtable.base('appelSu1QbwJdEw2p');
 
 async function publishMessage(id, text) {
 	try {
@@ -23,9 +32,41 @@ const hasUrl = (message) => (
 	).test(message)
 );
 
+const extractUrl = (message) => {
+	try {
+		let raw = message.match('([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?')[0];
+		let splitArr = ["|", ">"];
+		for (let symbol of splitArr) {
+			if (raw.includes(symbol)) {
+				return raw.split(symbol)[0];
+			}
+		}
+		return raw;
+	} catch (err) {
+		console.error(err);
+	}
+};
+
 app.event('message', async (body) => {
-	if (hasUrl(body.event.text)) {
-		publishMessage('C018J1ZEFA7', 'sick it works')
+	try {
+		if (typeof body.event.subtype === "undefined" && hasUrl(body.event.text)) {
+			let date = new Date(body.event.ts * 1000).toISOString();
+			let url = extractUrl(body.event.text);
+			let user = body.event.user;
+			base('test').create({
+				"date": date,
+				"url": url,
+				"user": user
+			}, function(err, record) {
+				if (err) {
+					console.error(err);
+					return;
+				}
+				console.log(record.getId());
+			});
+		}
+	} catch (err) {
+		console.error(err);
 	}
 });
 
@@ -36,4 +77,4 @@ app.event('message', async (body) => {
 	} catch (err) {
 		console.error(err);
 	}
-})()
+})();
